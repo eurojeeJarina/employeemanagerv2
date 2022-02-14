9e4aff304c2fdce517e929969b515da8860b0ed2
Eurojee Jarina <44507657+eurojeeJarina@users.noreply.github.com>
2022-02-14 07:49:08 +0000

    New project

---
+++ src/main/java/com/eurojee/employeemanagerv2/Employeemanagerv2Application.java
@@ -0,0 +1,13 @@
+package com.eurojee.employeemanagerv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Employeemanagerv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Employeemanagerv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/eurojee/employeemanagerv2/Employeemanagerv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurojee.employeemanagerv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Employeemanagerv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
